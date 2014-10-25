c655667795a8fee39c5c8ff36e5d9245b7d7ec3b
Abhishek Bhowmick <abhowmick22@gmail.com>
2014-10-25 13:56:23 -0400

    Faster mapping script

--- src/python/parseIDs.py
+++ src/python/parseIDs.py
@@ -16,24 +16,27 @@ for line in open('movies.dat', 'r'):
 # for each movie in summary corpus, write out the output file 
 # Format of output file is:
 # Wikipedia ID, MovieLens ID (None indicates no movie review present)
-with open('movie.metadata.tsv', 'r') as tsvin, open('IDmap.csv', 'ar+b') as csvfile:
+with open('movie.metadata.tsv', 'r') as tsvin:
+	csvfile = open('IDmap.csv', 'ar+')
 	tsvin = csv.reader(tsvin, delimiter = '\t')
 	csvout = csv.writer(csvfile, delimiter=',')
 
 	tsvRowNbr = 0;
 	moviesMatched = 0
+	lastwikiID = 0
 
-	# recover from checkpoint here
-	#logfile = open('IDmap.csv','r')
-	temp = csvfile.readlines()[-1].split(',')
-	lastwikiID = temp[0].rstrip() 
-
-	#logfile.close()
 	recovered = False
+	# recover from checkpoint here
+	log = csvfile.readlines()
+	if not log :
+		recovered = True
+	else :
+		temp = log[-1].split(',')
+		lastwikiID = temp[0].rstrip() 
 
 	for row in tsvin:
 		tsvRowNbr += 1
-		if tsvRowNbr <= 0 :
+		if tsvRowNbr <= 0 or tsvRowNbr >= 20000:
 			continue
 			
 		wikiID = row[0]
@@ -45,7 +48,10 @@ with open('movie.metadata.tsv', 'r') as tsvin, open('IDmap.csv', 'ar+b') as csvf
 
 		movieName = row[2]
 		# match this wikiID to the movielens ID using fuzzy match
-		for item in movielensIDs:
+		identifier = movieName[0];
+		movielensIDs = sorted(movielensIDs, key=lambda movie: movie[1])
+		movielensFiltered = [movie for movie in movielensIDs if movie[1][0] == identifier]
+		for item in movielensFiltered:
 			movielensName = item[1]
 			if nltk.metrics.edit_distance(movieName, movielensName) <= 1 :
 				moviesMatched += 1
