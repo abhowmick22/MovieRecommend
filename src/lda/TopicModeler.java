7b1def767de12975716be1feab82429cb10330d8
Abhishek Bhowmick <abhowmick22@gmail.com>
2014-10-28 21:53:28 -0400

    building layout 4

--- src/lda/TopicModeler.java
+++ src/lda/TopicModeler.java
@@ -1,13 +1,32 @@
 package lda;
 
+import java.util.ArrayList;
+
 /*
  * This is the main module that models the corpus, using variational
  * EM. This class provides the API that the CTR model interacts with.
  */
 
+/*
+ * The CTR client will first ask for the topic model, this contains the \alpha_k
+ * \beta_kXV matrix, which is list of topics, each topic being a distribution
+ * over words in the vocabulary (also returned).
+ * Next, it calls getFeatures for a new document - this returns the top
+ * k topics (features) of the document as a vector that
+ * will be used to characterise the document(movie). This works as follows -
+ * given \alpha and \beta as the parameters of the model, we infer the posterior
+ * Dirichlet parameters \gamma_k associated with the document. 
+ */
+
 public class TopicModeler {
 	
 
+	// Model a test document as list of topics
+	public ArrayList<Double> getFeatures(){
+		return null;
+		
+	}
+	
 	// Model the provided corpus
 	public Model modelCorpus(){
 		
@@ -19,7 +38,5 @@ public class TopicModeler {
 		
 		return null;
 	}
-	
-	// 
 	
 }
