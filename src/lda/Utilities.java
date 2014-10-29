984648c5f27103121d7a5370f04579d6d09cfbdc
Abhishek Bhowmick <abhowmick22@gmail.com>
2014-10-29 14:38:55 -0400

    Build layout 6

--- src/lda/Utilities.java
+++ src/lda/Utilities.java
@@ -1,10 +1,33 @@
 package lda;
 
+import java.util.List;
+
 /*
  * This module implements all the distributions, optimisations,
  * and other math utility functions.
  */
 
 public class Utilities {
-
+	
+	// Evaluate the stationary point of a function using Newton-Raphson method,
+	// assuming the form of the likelihood function is known (thus, it is not general)
+	// In LDA, this is used to maximize the loer bound of log L
+	// wrt \alpha and \beta in the M-step
+	public List<Double> optimizeNR(List<Double > init){
+		return null;
+	}
+	
+	
+	// This method evaluates the gradient of a vector
+	public List<Double> gradient(List<Double> point){
+		return null;
+	}
+	
+	
+	// This method evaluated the Hessian of a vector
+	public List<List<Double> > hessian(List<Double> point){
+		return null;
+	}
+	
+	
 }
