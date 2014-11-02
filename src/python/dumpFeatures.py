0e84d921f0d4d12dc8dc9440ebf253aa75cbec61
satwikkottur <satwik27@gmail.com>
2014-11-01 20:39:07 -0400

    Vocab sorted; numerical features dumped; ready for Document class

---
+++ src/python/dumpFeatures.py
@@ -0,0 +1,84 @@
+# Script to take processed summaries, dump them as a feature vector of indices with respect to the vocabulary
+import sys;
+import re;
+
+# Reading processed summaries and vocabulary 
+summaryFile = open('../../data/processed_summaries.txt', 'r');
+vocabFile = open('../../data/sortedVocab.txt', 'r');
+
+# Checking for existance of files
+if(summaryFile == None or vocabFile == None):
+    sys.exit('Summary file / Vocabulary file not found\n');
+   
+# Reading summary and vocabulary files
+# Stripping \n sign
+summaryLines = [s.rstrip("\n\r") for s in summaryFile.readlines()]
+vocabLines = [s.rstrip("\n\r") for s in vocabFile.readlines()]
+
+# Closing files
+summaryFile.close();
+vocabFile.close();
+
+# Compute the index based on starting letter
+words = [];
+
+# Initialize the index with 'a' at 0
+index = {'a':0};
+
+# Next search letter
+searchLetter = 'b';
+
+# Collecting words
+words = [];
+
+for i in vocabLines:
+    splits = i.split(',');
+    words.append(splits[1]);
+
+    # Indexing
+    curLetter = splits[1][0];
+    if(curLetter == searchLetter):
+        index[searchLetter] = vocabLines.index(i);
+        searchLetter = chr(ord(searchLetter) + 1);
+
+# Appending a dummy entry for handling 'z' and its next ascii character '{'
+index['{'] = len(words);
+
+# Opening a file to dump feature for the movie plots
+featureFile = open('../../data/summaryFeatures.txt', 'wb');
+
+if(featureFile == None):
+    sys.exit('Feature file not found\n');
+
+# For each movie summary find the indices of words and dump them as feature vectors
+iterId = 0;
+for line in summaryLines:
+    feature = [];
+
+    lineSplit = line.split(',');
+    movieId = int(lineSplit[0]);
+
+    # Debug message
+    iterId = iterId + 1;
+    if(iterId % 100 == 0):
+        print iterId
+
+    # Reading the set of words using regular expressions
+    string = re.findall(r'\[([^]]*)\]', line);
+    string = re.findall('\'([^\']*)\'', string[0]);
+
+    for i in string:
+        if(len(i) < 1):
+            continue;
+
+        startLetter = i[0];
+        # Word present in the vocabulary 
+        if(ord(startLetter) > 96 and ord(startLetter) < 123):
+            subWordList = words[index[startLetter]:index[chr(ord(startLetter)+1)]];
+            if(i in subWordList):
+                feature.append(index[startLetter] + subWordList.index(i));
+   
+    # Dumping the feature vector into the file
+    featureFile.write(str(movieId) + ', ' +  str(feature) + '\n');
+
+featureFile.close()
---
+++ src/python/sortDictionary.py
@@ -0,0 +1,36 @@
+# Script to sort the vocabulary; can be used to add additional rules later.
+import sys;
+
+# Reading processed summaries and vocabulary 
+vocabFile = open('../../data/vocab.txt', 'r');
+
+# Checking for existance of files
+if(vocabFile == None):
+    sys.exit('Vocabulary file not found\n');
+   
+# Reading vocabulary files
+# Stripping \n sign
+vocabLines = [s.rstrip("\n\r") for s in vocabFile.readlines()]
+
+# Closing files
+vocabFile.close();
+
+# Sorting the vocabulary
+# Extracting words and sorting them
+words = [];
+for i in vocabLines:
+    splits = i.split(',');
+    # Weed out words that dont start with an alphabet
+    if(ord(splits[1][0]) > 96 and ord(splits[1][0]) < 123):
+        words.append(splits[1]);
+
+#sys.exit();
+words.sort()
+
+# Dumping the new dictionary
+newDictFile = open('../../data/sortedVocab.txt', 'w');
+for i in xrange(1, len(words)):
+    newLine = str(i) + ',' + words[i] + '\n';
+    newDictFile.write(newLine)
+    
+newDictFile.close();
