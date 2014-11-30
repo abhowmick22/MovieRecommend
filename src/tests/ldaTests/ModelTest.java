359c64786710dad068bed200de0899caee06686d
satwikkottur <satwik27@gmail.com>
2014-11-30 00:30:17 -0500

    Reading model from file succesful :)

--- src/tests/ldaTests/ModelTest.java
+++ src/tests/ldaTests/ModelTest.java
@@ -15,8 +15,8 @@ public class ModelTest {
 	/************************************************************/
 	public static void main(String[] args){
 		// Path to the file
-		String corpusPath = "data/summariesTrain.txt";
-		String vocabPath = "data/sortedVocab.txt";
+		String corpusPath = "data/summaries_debug_big_nostemming.txt";
+		String vocabPath = "data/sortedVocab_nostemming.txt";
 		
 		Corpus movieSummaries;
 		Vocabulary movieVocab;
@@ -30,7 +30,7 @@ public class ModelTest {
 		
 		// Create the configs
 		Configs conf = new Configs();
-		conf.setNbrTopics(10);
+		conf.setNbrTopics(20);
 		
 		// Initialize the model
 		Model model = new Model();
@@ -41,21 +41,8 @@ public class ModelTest {
 		System.out.println("Vocab size: " + model.getVocabSize());
 		System.out.println("Nbr of reviews: " + movieSummaries.getNbrDocs());
 		
-		File dump = new File("src/tests/ldaTests/modelDump.txt");
-		try {
-			PrintWriter writer = new PrintWriter(dump, "UTF-8");
-			System.out.println("Size of alpha is: " + model.getAlpha().getDimension());
-			System.out.println("Size of beta is: " + model.getBeta().getRowDimension() + 
-									" X " + model.getBeta().getColumnDimension());
-			writer.println("Hyperparameters:\n\n");
-			writer.println("Initial alpha: " + model.getAlpha());
-			writer.println("Initial beta: " + model.getBeta());
-		
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
+		// Reading from the file
+		model.readModelFromFile("src/tests/ldaTests/modelDumpMethod.txt");
 		
 	}
 	/***************************************************************/
